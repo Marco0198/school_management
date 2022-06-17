c28dbed515c700bf3cdb016cc8e9505b74fb7b28
Marco0198 <81382003+Marco0198@users.noreply.github.com>
2022-06-17 22:18:30 +0200

    employee classes domain

--- src/main/java/ac/za/mycput/domain/employee/Employee.java
+++ src/main/java/ac/za/mycput/domain/employee/Employee.java
@@ -3,20 +3,70 @@ package ac.za.mycput.domain.employee;
 
 
 import ac.za.mycput.domain.name.Name;
+import ac.za.mycput.domain.student.Student;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
+import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
 
 public class Employee {
-    private String staffId,email;
-
-    public Name getName() {
-        return name;
-    }
-
+    @Id
+    @NotNull
+    private String staffId;
+    @NotNull private String  email;
     @Embedded
     private Name name;
+    public Name getName() {return name;
+    }
 
-    protected Employee() {
+    public Employee(Builder builder) {
+        this.staffId= builder.staffId;
+        this.email= builder.email;
+    }
+    public Employee() {}
+
+    public String getStafftId() {return staffId;}
+    public String getStudentEmail() {return email;}
+
+
+    public static class Builder{
+        @Id
+        @Column(name="student_id")
+        private String staffId;
+        @Column(name="email")
+
+        private String  email;
+
+        public Builder setStudentId(String studentId){
+            this.staffId= studentId;
+            return this;
+        }
+
+
+        public Builder setEmail(String email){
+            this.email= email;
+            return this;
+        }
+
+        public Builder copy(Student student){
+
+            this.staffId=staffId;
+            this.email=email;
+
+            return this;
+        }
+        public Employee build(){
+            return new Employee(this);
+        }
     }
 }
+
+
+
+
+
+
+
+
